cc49bcbeeacc3a10331ce53924e692e8ad91e17a
WahabDev12 <nerdyshinobi12@gmail.com>
2022-09-09 14:44:57 +0000

    feat: change config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    "global": {},
+    "global": '({})',
   },
   plugins: [react()] 
 
